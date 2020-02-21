1c12f2344778e316002d90a4835308a1f5aaac53
lijianru <2219269374@qq.com>
2020-02-21 21:04:16 +0800

    add mongoose

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -8,11 +8,12 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+import { PostModel } from './post.model';
 
 class CreatePostDTO {
-  @ApiProperty({ description: '帖子的标题' })
+  @ApiProperty({ description: '帖子的标题', example: '帖子的标题' })
   title: string;
-  @ApiProperty({ description: '帖子的内容' })
+  @ApiProperty({ description: '帖子的内容', example: '帖子的内容' })
   content: string;
 }
 
@@ -21,14 +22,14 @@ class CreatePostDTO {
 export class PostsController {
   @Get()
   @ApiOperation({ description: '获取帖子列表' })
-  index() {
-    return [];
+  async index() {
+    return await PostModel.find();
   }
 
   @Post()
   @ApiOperation({ description: '创建帖子' })
-  create(@Body() body: CreatePostDTO) {
-    console.log(body);
+  async create(@Body() createPostDTO: CreatePostDTO) {
+    await PostModel.create(createPostDTO);
     return {
       success: true,
     };
@@ -36,27 +37,24 @@ export class PostsController {
 
   @Get(':id')
   @ApiOperation({ description: '获取帖子详情' })
-  detail(@Param('id') id: string) {
-    return {
-      id,
-      title: '第一篇文章',
-    };
+  async detail(@Param('id') id: string) {
+    return await PostModel.findById(id);
   }
 
   @Put(':id')
   @ApiOperation({ description: '编辑帖子' })
-  update(@Param('id') id: string, @Body() body: CreatePostDTO) {
+  async update(@Param('id') id: string, @Body() updatePostDTO: CreatePostDTO) {
+    await PostModel.findByIdAndUpdate(id, updatePostDTO);
     return {
-      id,
-      title: body.title,
+      success: true,
     };
   }
 
   @Delete(':id')
   @ApiOperation({ description: '删除帖子' })
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
+    await PostModel.findByIdAndDelete(id);
     return {
-      id,
       success: true,
     };
   }
